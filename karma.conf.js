d69bcf5b7ffad668b77bc12fcd98cf19ab86b970
Seth Messer <seth.messer@gmail.com>
2014-08-09 22:34:41 -0500

    Change back to phantomjs runner

--- karma.conf.js
+++ karma.conf.js
@@ -56,8 +56,8 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
-      // 'PhantomJS',
-      'Chrome',
+      'PhantomJS',
+      // 'Chrome',
       // 'Firefox',
       // 'Safari'
     ],
